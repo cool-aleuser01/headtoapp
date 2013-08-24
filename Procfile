7070bb87906adca3ffcec47d2bff8b7c5b8005a8
Param Aggarwal <paramaggarwal@gmail.com>
2013-08-24 17:52:28 +0530

    Stop talking to FB on loading index page

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 #commented code for the thin server
 #web: bundle exec thin -R config.ru start -p $PORT -e ${RACK_ENV:-development}
 web: bundle exec unicorn -p $PORT -c ./unicorn.rb
+
--- app.rb
+++ app.rb
@@ -1,22 +1,12 @@
 require "sinatra"
 require 'koala'
 require "foursquare2"
-require "net/https"
-require "uri"
-require "fiber"
 
 enable :sessions
 set :raise_errors, false
 set :show_exceptions, false
 
-# Scope defines what permissions that we are asking the user to grant.
-# In this example, we are asking for the ability to publish stories
-# about using the app, access to what the user likes, and to be able
-# to use their pictures.  You should rewrite this scope with whatever
-# permissions your app needs.
-# See https://developers.facebook.com/docs/reference/api/permissions/
-# for a full list of permissions
-FACEBOOK_SCOPE = 'user_likes,user_photos,publish_actions'
+FACEBOOK_SCOPE = 'publish_actions'
 
 unless ENV["FACEBOOK_APP_ID"] && ENV["FACEBOOK_SECRET"]
   abort("missing env vars: please set FACEBOOK_APP_ID and FACEBOOK_SECRET with your app credentials")
@@ -65,17 +55,6 @@ helpers do
     session[:access_token] || access_token_from_cookie
   end
 
-  def hit_object_to_cache(object)
-
-    url = URI.parse('http://developers.facebook.com/tools/debug/og/object')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    req = Net::HTTP::Post.new(url.request_uri)
-    req.set_form_data({"q" => object})
-    http.request(req)
-  end
-
 end
 
 # the facebook session expired! reset ours and restart the process
@@ -85,21 +64,6 @@ error(Koala::Facebook::APIError) do
 end
 
 get "/" do
-  # Get base API Connection
-  @graph  = Koala::Facebook::API.new(access_token)
-
-  # Get public details of current application
-  @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
-
-  if access_token
-    @user    = @graph.get_object("me")
-    @friends = @graph.get_connections('me', 'friends')
-    @photos  = @graph.get_connections('me', 'photos')
-    @likes   = @graph.get_connections('me', 'likes').first(4)
-
-    # for other data you can always run fql
-    @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
-  end
   erb :index
 end
 
@@ -130,7 +94,6 @@ get "/object/:location_id" do
 end
 
 get "/postAction/:location_id" do
-
   begin
     @graph  = Koala::Facebook::API.new(access_token)
     object_url = "http://headtoapp.herokuapp.com/object/" + params[:location_id]
