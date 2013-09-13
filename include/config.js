409d344bc3bed45d8fa5d9ac2bf7ab274d518917
Param Aggarwal <paramaggarwal@gmail.com>
2013-09-13 23:05:23 +0530

    Exposed config file

---
+++ include/config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.facebookAppId = "";
+config.facebookSecret = "";
+
+config.foursquareClientId = "EBA5MTZIPRVHNGKU2RB4KZ45J2BAOZFSYIXHGYBGR1KIXFIQ";
+config.foursquareClientSecret = "K0CBX5TQKHNEB35MGT3NNIVLWP0C4L0CQQ4UP3C2LUSLQL0W";
+
+module.exports = config;
