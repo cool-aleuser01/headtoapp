f4636843d37e88cc6f1ff0866d1633a34905300c
Param Aggarwal <paramaggarwal@gmail.com>
2013-09-13 23:13:25 +0530

    Modified port number for Heroku

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/venue/:id', function(request, response) {
   foursquare.getVenue(request.params.id, response);
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
