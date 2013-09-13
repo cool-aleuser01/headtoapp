403136cbce1f23a43cd1084846c9c56ef6f85c06
Param Aggarwal <paramaggarwal@gmail.com>
2013-09-13 20:44:44 +0530

    Moved Foursquare aspects to own library

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var https = require('https');
-var config = require('./config')
+var config = require('./include/config')
+var foursquare = require('./include/foursquare')
 
 var app = express();
 app.set('view engine', 'hjs');
@@ -15,32 +16,8 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
-function getVenue(id, response) {
-  var foursquareURL = "https://api.foursquare.com/v2/venues/"
-  + id + "?v=20130725"
-  + "&client_id="+ config.foursquareClientId
-  + "&client_secret=" + config.foursquareClientSecret;
-  
-  https.get(foursquareURL, function(res) {
-    var body = '';
-
-    res.on('data', function(chunk) {
-        body += chunk;
-    });
-
-    res.on('end', function() {
-        var venueObject = JSON.parse(body).response;
-        console.log(JSON.stringify(venueObject));
-        response.render('venue', venueObject);
-    });
-
-  }).on('error', function(e) {
-    console.log("Got error: " + e.message);
-  });
-}
-
 app.get('/venue/:id', function(request, response) {
-  getVenue(request.params.id, response);
+  foursquare.getVenue(request.params.id, response);
 });
 
 var port = process.env.PORT || 3000;
---
+++ include/foursquare.js
@@ -0,0 +1,30 @@
+var https = require('https');
+var config = require('./config')
+
+var foursquare = {};
+
+foursquare.getVenue = function (id, response) {
+  var foursquareURL = "https://api.foursquare.com/v2/venues/"
+  + id + "?v=20130725"
+  + "&client_id="+ config.foursquareClientId
+  + "&client_secret=" + config.foursquareClientSecret;
+  
+  https.get(foursquareURL, function(res) {
+    var body = '';
+
+    res.on('data', function(chunk) {
+        body += chunk;
+    });
+
+    res.on('end', function() {
+        var venueObject = JSON.parse(body).response;
+        console.log(JSON.stringify(venueObject));
+        response.render('venue', venueObject);
+    });
+
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+}
+
+module.exports = foursquare;
